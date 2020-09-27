7eea84d79243842ac0fb361540f723b9e6b4e2ba
Soleiu <himamegu847@gmail.com>
2020-09-27 22:58:37 +0900

    test

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,13 @@
 class ItemsController < ApplicationController
+  before_action :index_category_set,only::index
+
+  private
+
+  def index_category_set
+    array = [1,2,3,4,5]
+      for num in array do
+    end
+
   def index
   end
 
